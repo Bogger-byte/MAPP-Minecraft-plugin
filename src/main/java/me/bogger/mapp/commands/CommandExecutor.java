7ff6c2c15a592e9231904bb604ea6e195099fa38
Bogger-byte <nik.troshnev@gmail.com>
2022-02-22 18:40:14 +0700

    Deleted due package moving or refactoring

--- src/main/java/me/bogger/mapp/commands/CommandExecutor.java
+++
@@ -1,8 +0,0 @@
-package me.bogger.mapp.commands;
-
-import org.bukkit.command.CommandSender;
-import org.jetbrains.annotations.NotNull;
-
-public interface CommandExecutor {
-    boolean execute(@NotNull CommandSender sender, String[] args);
-}
